b219587733d2395b14bbd7db063e931555168aa2
u0693875-billprisbrey <u0693875@umail.utah.edu>
2024-09-16 23:15:04 +0000

    Filled the dots according to a calendar of the days of the months

--- Random Dots.R
+++ Random Dots.R
@@ -7,6 +7,7 @@ dotsPlot <- function(){plot(sample(1:100, 20),
                  sample(1:100, 20),
                  col = viridis::viridis(20),
                  cex = sample(1:20),
+                 pch = sample(c(rep(1,10),19), 20, replace = TRUE),
                  xlab = "",
                  ylab = "",
                  xaxt = "n",
@@ -22,11 +23,24 @@ dotsPlot2 <- function(){
   # Exclude read-only parameters
   incomingPar <- incomingPar[!names(incomingPar) %in% c("cin", "cra", "csi", "cxy", "din", "page")]
   
+  # fill circles according to day of the month (a kind of count-down calendar)
+  
+  max_days <- Sys.Date() |> 
+         (\(x) as.Date(cut(x, "month")))() |> 
+         (\(x) as.Date(cut(x + 31, "month")) - 1)() |> 
+         (\(x) as.numeric(format(x, "%d")))()
+  
+  current_day <- as.numeric(format(Sys.Date(), "%d"))
+  
+  shapeFill <- rep(1,max_days)
+  shapeFill[1:current_day] <- 19
+  
   par(mar = c(0,0,0,0))
-  plot(sample(1:100, 20),
-                            sample(1:100, 20),
-                            col = viridis::viridis(20),
-                            cex = sample(1:20),
+  plot(sample(1:100, max_days),
+                            sample(1:100, max_days),
+                            col = viridis::viridis(max_days),
+                            cex = sample(1:max_days),
+                            pch = shapeFill,
                             bty = "n",
                             xlab = "",
                             ylab = "",
@@ -40,4 +54,10 @@ dotsPlot2 <- function(){
 
 # next, I want to email this to me.
 # Some daily check that everything is working as it should.
+
+# It would also be nice to turn some of these into solid plots.
+
+# And I'm working on filling up the dots
+
+# Now I've decided to fill up the dots according to a calendar of the days
 
