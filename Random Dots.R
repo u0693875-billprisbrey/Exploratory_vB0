215fbd68d12150efa29d36a1a9ac22c12fee374b
u0693875-billprisbrey <u0693875@umail.utah.edu>
2024-09-16 22:42:47 +0000

    Resets the parameters after creating the random circle plot.

--- Random Dots.R
+++ Random Dots.R
@@ -17,15 +17,27 @@ dotsPlot <- function(){plot(sample(1:100, 20),
 
 dotsPlot2 <- function(){
   
+  incomingPar <- par()
+  
+  # Exclude read-only parameters
+  incomingPar <- incomingPar[!names(incomingPar) %in% c("cin", "cra", "csi", "cxy", "din", "page")]
+  
   par(mar = c(0,0,0,0))
   plot(sample(1:100, 20),
                             sample(1:100, 20),
                             col = viridis::viridis(20),
                             cex = sample(1:20),
+                            bty = "n",
                             xlab = "",
                             ylab = "",
                             xaxt = "n",
                             yaxt = "n"
 )
+  
+  par(incomingPar)
+  
 }
+
+# next, I want to email this to me.
+# Some daily check that everything is working as it should.
 
