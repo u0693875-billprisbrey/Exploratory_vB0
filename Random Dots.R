1489c874de3bb791f643eb48d4cb51ed6a9ddf2c
u0693875-billprisbrey <u0693875@umail.utah.edu>
2024-09-16 21:39:21 +0000

    Making a nice random dots plot

---
+++ Random Dots.R
@@ -0,0 +1,11 @@
+# A second file
+# 9.16.2024
+
+# The purpose of this file is to practice with pulling and pushing branches.
+
+dotsPlot <- function(){plot(sample(1:100, 20),
+                 sample(1:100, 20),
+                 col = viridis::viridis(20),
+                 cex = sample(1:20)
+                 )
+}
--- Silly File vA0.R
+++ Silly File vA0.R
@@ -6,3 +6,8 @@ plot((1:10)^1.75)
 
 # adding text to test the revert
 plot((10:100)^1.33)
+
+# So it looks like "revert" only goes back ONE save
+# We don't have the Git panel
+# And we are struggling to pull back an earlier version.
+
